f8a4c48a1cb4a4c8b269b5b2a8f3daea535a38fc
Daniela <roehrig.87.daniela@gmx.de>
2025-06-10 14:52:34 +0200

    corrected ModalWindowContent

--- src/modules/ModalWindow/ModalWindowContent.jsx
+++ src/modules/ModalWindow/ModalWindowContent.jsx
@@ -1,39 +1,6 @@
 import WhiteCross from '../../shared/Icons/images/whiteCross.png';
 import styles from './ModalWindow.module.css';
 
-const ModalWindowContent = ({ onClose }) => {
-    return (
-        <div className={styles.modalWin} onClick={(e) => e.stopPropagation()}>
-            <button className={styles.closeWin} onClick={onClose} aria-label="Close Modal">
-                <img src={WhiteCross} alt="Close" />
-            </button>
-            <div className={styles.congratulation}>
-                <h2
-                    style={{
-                        color: 'white',
-                        fontSize: '40px',
-                        fontWeight: '600',
-                    }}
-                >
-                    Congratulations!
-                </h2>
-                <p style={{ paddingTop: '20px' }}>
-                    Your order has been successfully placed on the website.
-                </p>
-                <p style={{ paddingTop: '20px' }}>
-                    A manager will contact you shortly to confirm your order.
-                </p>
-            </div>
-        </div>
-    );
-};
-
-export default ModalWindowContent;
-
-
-/* import WhiteCross from '../../shared/Icons/images/whiteCross.png';
-import styles from './ModalWindow.module.css';
-
 const ModalWindowContent = ({ onClose, title, firstParagraph, secondParagraph }) => {
     return (
         <div className={styles.modalWin} onClick={(e) => e.stopPropagation()}>
@@ -62,4 +29,3 @@ const ModalWindowContent = ({ onClose, title, firstParagraph, secondParagraph })
 };
 
 export default ModalWindowContent;
- */
